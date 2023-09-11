30a6235038d78e90c1cdbcb5f0eeac2fe08de01f
Crazelu <eberelucky64@gmail.com>
2023-09-11 22:47:50 +0100

    Extracts views and adds improvements for week 2 assignment

--- week2/starter/ColorPicker/Views/ColoredRectangle.swift
+++ week2/starter/ColorPicker/Views/ColoredRectangle.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco Inc.
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,46 +32,24 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State private var redColor: Double = 0.0
-  @State private var greenColor: Double = 0.0
-  @State private var blueColor: Double = 0.0
-  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
-  @Environment(\.colorScheme) var colorScheme
-  @Environment(\.verticalSizeClass) var verticalSizeClass
+struct ColoredRectangle: View {
+  @Binding var foregroundColor: Color
   
-  var isInDarkMode: Bool {
-    colorScheme == .dark
-  }
-  
-  var isInPortraitMode: Bool {
-    verticalSizeClass == .regular
-  }
-  
-  var body: some View {
-    ResponsiveView(
-      redColor: $redColor,
-      greenColor: $greenColor,
-      blueColor: $blueColor,
-      foregroundColor: $foregroundColor,
-      isInPortraitMode: isInPortraitMode
-    )
-    .background(isInDarkMode ? Color.black : Color.white)
-    .padding(Constants.padding)
-  }
+    var body: some View {
+      RoundedRectangle(cornerRadius: 0)
+        .fill(foregroundColor)
+        .overlay {
+          RoundedRectangle(cornerRadius: 0)
+            .strokeBorder(Color.gray.opacity(0.3), lineWidth: 10)
+        }
+    }
 }
 
-
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-    ContentView()
-      .preferredColorScheme(.dark)
-    ContentView()
-      .previewInterfaceOrientation(.landscapeRight)
-    ContentView()
-      .preferredColorScheme(.dark)
-      .previewInterfaceOrientation(.landscapeRight)
-  }
+struct ColoredRectangle_Previews: PreviewProvider {
+  static private var foregroundColor = Binding.constant(
+    Color(red: 0, green: 0, blue: 0)
+  )
+    static var previews: some View {
+        ColoredRectangle(foregroundColor: foregroundColor)
+    }
 }
--- week2/starter/ColorPicker/Views/SetColorButton.swift
+++ week2/starter/ColorPicker/Views/SetColorButton.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco Inc.
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,46 +32,32 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State private var redColor: Double = 0.0
-  @State private var greenColor: Double = 0.0
-  @State private var blueColor: Double = 0.0
-  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
-  @Environment(\.colorScheme) var colorScheme
-  @Environment(\.verticalSizeClass) var verticalSizeClass
+struct SetColorButton: View {
+  var action: () -> Void
   
-  var isInDarkMode: Bool {
-    colorScheme == .dark
-  }
-  
-  var isInPortraitMode: Bool {
-    verticalSizeClass == .regular
-  }
-  
-  var body: some View {
-    ResponsiveView(
-      redColor: $redColor,
-      greenColor: $greenColor,
-      blueColor: $blueColor,
-      foregroundColor: $foregroundColor,
-      isInPortraitMode: isInPortraitMode
-    )
-    .background(isInDarkMode ? Color.black : Color.white)
-    .padding(Constants.padding)
-  }
+    var body: some View {
+      Button{
+        action()
+      } label: {
+        Text("Set Color")
+          .padding(Constants.padding)
+          .background(Constants.AppColors.buttonColor)
+          .foregroundColor(.white)
+          .cornerRadius(Constants.cornerRadius)
+          .overlay {
+            RoundedRectangle(cornerRadius: Constants.cornerRadius)
+              .strokeBorder(Constants.AppColors.buttonBorderColor, lineWidth: 2)
+          }
+      }
+     
+      
+    }
 }
 
-
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-    ContentView()
+struct SetColorButton_Previews: PreviewProvider {
+    static var previews: some View {
+      SetColorButton { }
+      SetColorButton { }
       .preferredColorScheme(.dark)
-    ContentView()
-      .previewInterfaceOrientation(.landscapeRight)
-    ContentView()
-      .preferredColorScheme(.dark)
-      .previewInterfaceOrientation(.landscapeRight)
-  }
+    }
 }
